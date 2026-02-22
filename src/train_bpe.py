1be6636ae318a1f389656101f5cc0105dd025706
janahmedprg <jan.ahmed.prg@gmail.com>
2026-02-21 20:10:10 -0500

    optimized bpe using max heap

--- src/train_bpe.py
+++ src/train_bpe.py
@@ -1,6 +1,7 @@
 from cs336_basics.pretokenization_example import find_chunk_boundaries
 import regex as re
 from multiprocessing import Pool
+from src.bp_max_heap import BytePairMaxHeap
 import os
 
 class BytePairMap:
@@ -14,18 +15,14 @@ class BytePairMap:
                 byte_pair = (bytes([key[i]]), bytes([key[i+1]]))
                 self.byte_pairs_freq[byte_pair] = self.byte_pairs_freq.get(byte_pair, 0) + pre_token_dict[key]
                 self.byte_pairs_indices.setdefault(byte_pair, set()).add(key)
+        
+        self.byte_pairs_heap = BytePairMaxHeap(self.byte_pairs_freq)
 
-    def getMaxFreq(self) -> tuple[bytes, bytes]:
-        maxKey = (b"", b"")
-        maxVal = 0 
-        for key, value in self.byte_pairs_freq.items():
-            if maxVal < value:
-                maxKey = key
-                maxVal = value
-            elif maxVal == value and maxKey < key:
-                maxKey = key
-        return maxKey
-
+    def getMaxFreq(self):
+        while len(self.byte_pairs_heap.bp_heap):
+            max_bp = self.byte_pairs_heap.pop()
+            if max_bp[0] in self.byte_pairs_freq and max_bp[1] == self.byte_pairs_freq[max_bp[0]]:
+                return max_bp[0]
     
     def getIndices(self, byte_pair: tuple[bytes, bytes]) -> list[tuple[int, ...]]:
         return self.byte_pairs_indices[byte_pair]
@@ -35,10 +32,13 @@ class BytePairMap:
         if self.byte_pairs_freq[byte_pair] == 0:
             del self.byte_pairs_freq[byte_pair]
             del self.byte_pairs_indices[byte_pair]
+        else:
+            self.byte_pairs_heap.push((byte_pair, self.byte_pairs_freq[byte_pair]))
     
     def addBytePair(self, byte_pair: tuple[bytes, bytes], freq: int, index: str):
         self.byte_pairs_freq[byte_pair] = freq + self.byte_pairs_freq.get(byte_pair, 0)
         self.byte_pairs_indices.setdefault(byte_pair, set()).add(index)
+        self.byte_pairs_heap.push((byte_pair, self.byte_pairs_freq[byte_pair]))
     
     def deleteBytePair(self, byte_pair: tuple[bytes, bytes]):
         del self.byte_pairs_freq[byte_pair]
@@ -163,10 +163,10 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 file_path = os.path.join(script_dir, "..", "data/owt_train.txt")
 
-bpe = train_bpe(file_path, 10000, ["<|endoftext|>"])
+bpe = train_bpe(file_path, 32000, ["<|endoftext|>"])
 
-print(max(bpe[0].values(), key=len))
+# print(max(bpe[0].values(), key=len))
 
-with open('bpe_output.txt', 'w') as f:
+with open('bpe_output_test.txt', 'w') as f:
     f.write('Vocabulary = ' + str(bpe[0]) + '\n')
     f.write('Merges = ' + str(bpe[1]) + '\n')
